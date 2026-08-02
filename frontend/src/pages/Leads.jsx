2c47d564826a0e1cf8f787cbe155c1d44e77d778
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-02 23:57:04 +0200

    Create leads page

---
+++ frontend/src/pages/Leads.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+function Leads(){
+
+
+return (
+
+<div>
+
+<h1>
+Prospección Comercial
+</h1>
+
+
+
+<div className="card">
+
+
+<p>
+Empresas potenciales y oportunidades comerciales
+</p>
+
+
+
+<button>
+Nueva oportunidad
+</button>
+
+
+</div>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Leads;
