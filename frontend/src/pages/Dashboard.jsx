6545432a35d2aa1bed1b611520d22aca42afb9a0
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-02 23:54:32 +0200

    Create dashboard page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+
+function Dashboard(){
+
+
+return (
+
+<div>
+
+<h1>
+Dashboard
+</h1>
+
+
+<div className="dashboard">
+
+
+<div className="stat">
+
+<h3>0</h3>
+
+<p>👥 Clientes</p>
+
+</div>
+
+
+
+<div className="stat">
+
+<h3>0</h3>
+
+<p>📈 Leads</p>
+
+</div>
+
+
+
+<div className="stat">
+
+<h3>0</h3>
+
+<p>💰 Presupuestos</p>
+
+</div>
+
+
+
+<div className="stat">
+
+<h3>0</h3>
+
+<p>📦 Pedidos</p>
+
+</div>
+
+
+
+</div>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Dashboard;
