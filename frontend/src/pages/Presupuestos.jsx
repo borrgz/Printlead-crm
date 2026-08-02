c78ce86f7b134225c076f070416116a4437b9426
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-02 23:59:49 +0200

    Create quotes page

---
+++ frontend/src/pages/Presupuestos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+function Presupuestos(){
+
+
+return (
+
+<div>
+
+
+<h1>
+Presupuestos
+</h1>
+
+
+
+<div className="card">
+
+
+<p>
+Gestión de presupuestos comerciales de PrintLead CRM.
+</p>
+
+
+
+<button>
+Nuevo presupuesto
+</button>
+
+
+</div>
+
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Presupuestos;
