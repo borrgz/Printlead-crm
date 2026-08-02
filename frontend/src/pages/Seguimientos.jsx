f660f8ec3a78f86d7e5e177a49ad952442e8447f
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-02 23:58:14 +0200

    Create followups page

---
+++ frontend/src/pages/Seguimientos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+function Seguimientos(){
+
+
+return (
+
+<div>
+
+
+<h1>
+Seguimientos Comerciales
+</h1>
+
+
+
+<div className="card">
+
+
+<p>
+Control de llamadas, visitas y contactos con clientes.
+</p>
+
+
+
+<button>
+Nuevo seguimiento
+</button>
+
+
+</div>
+
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Seguimientos;
