522ab4030712657fdb6ccecdd69317f50137ded7
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-03 00:01:03 +0200

    Create orders page

---
+++ frontend/src/pages/Pedidos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+function Pedidos(){
+
+
+return (
+
+<div>
+
+
+<h1>
+Pedidos y Producción
+</h1>
+
+
+
+<div className="card">
+
+
+<p>
+Control de trabajos en producción e instalaciones.
+</p>
+
+
+
+<button>
+Nuevo pedido
+</button>
+
+
+</div>
+
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Pedidos;
