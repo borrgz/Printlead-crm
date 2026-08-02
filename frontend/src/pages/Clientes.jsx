046279c50456d66751af31475c715e28638c474d
borrgz <132871589+borrgz@users.noreply.github.com>
2026-08-02 23:55:52 +0200

    Create clients page

---
+++ frontend/src/pages/Clientes.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+function Clientes(){
+
+
+return (
+
+<div>
+
+<h1>
+Clientes
+</h1>
+
+
+<div className="card">
+
+<p>
+Gestión de clientes de PrintLead CRM
+</p>
+
+
+<button>
+Nuevo cliente
+</button>
+
+
+</div>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Clientes;
